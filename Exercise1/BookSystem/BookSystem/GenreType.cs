6b8a914a21ca342f6337afdcfd7f7c726c20e52d
lsingh988 <157623878+lsingh988@users.noreply.github.com>
2024-09-29 16:12:17 -0600

    commit
    commit done

---
+++ Exercise1/BookSystem/BookSystem/GenreType.cs
@@ -0,0 +1,12 @@
+﻿namespace BookSystem
+{
+    public enum GenreType
+    {//enum for the different types of genres
+        Fiction,
+        NonFiction,
+        Fantasy,
+        Biography,
+        Mystery,
+        Romance
+    }
+}
